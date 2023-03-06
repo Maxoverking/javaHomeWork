9adede6738b9eacf3c0741b4a9a574c60f114f8a
Maxoverking <avemax7@gmail.com>
2023-03-06 01:10:10 +0100

    Clean Up

--- Dict/HW_Dict.java
+++ Dict/HW_Dict.java
@@ -1,41 +1,54 @@
 package Dict;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class HW_Dict {
 
     public static void main(String[] args) throws IOException {
-        Map<String, String> dictionary = new HashMap<>();
 
-        BufferedReader readFile = new BufferedReader(new FileReader("dict.txt"));
-        int n = Integer.parseInt(readFile.readLine());
-        for (int i = 0; i < n; i++) {
-            String line = readFile.readLine();
-            String[] divideLine = line.split(": ");
-            dictionary.put(divideLine[0], divideLine[1]);
+        Map<String, String> dictionary = readFileAndMapIt();
+
+        List<String> inputWrds = inputValue();
+        for (String word : inputWrds) {
+            if (dictionary.keySet().contains(word)) {
+                System.out.println(dictionary.get(word));
+            } else {
+                System.out.println("Не найдено");
+            }
         }
-        readFile.close();
+    }
 
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        List<String> inputWords = new ArrayList<>();
-        int m = Integer.parseInt(read.readLine());
-        for (int i = 0; i < m; i++) {
-            String line = read.readLine();
-            inputWords.add(line);
-            // if (dictionary.keySet().contains(line)) {
-            // System.out.println(dictionary.get(line));
-            // }
+    public static Map<String, String> readFileAndMapIt() {
+
+        try (BufferedReader readFile = new BufferedReader(new FileReader("dict.txt"))) {
+            Map<String, String> dictionary = new HashMap<>();
+            int n = Integer.parseInt(readFile.readLine());
+
+            for (int i = 0; i < n; i++) {
+                String[] convertToArray = readFile.readLine().split(": ");
+                dictionary.put(convertToArray[0].toLowerCase(), convertToArray[1]);
+            }
+            return dictionary;
+        } catch (NumberFormatException | IOException e) {
+            e.printStackTrace();
         }
-        read.close();
-        System.out.println("ввели" + inputWords);
+        return null;
+    }
 
+    public static List<String> inputValue() {
+        try (BufferedReader read = new BufferedReader(new InputStreamReader(System.in))) {
+
+            List<String> inputWords = new ArrayList<>();
+
+            int m = Integer.parseInt(read.readLine());
+            for (int i = 0; i < m; i++) {
+                inputWords.add(read.readLine().toLowerCase());
+            }
+            return inputWords;
+        } catch (NumberFormatException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
