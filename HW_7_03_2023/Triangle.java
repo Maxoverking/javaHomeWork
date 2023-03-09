9f96b782d7f158fcc26805abaa20619e651924b1
Maxoverking <avemax7@gmail.com>
2023-03-09 13:33:05 +0100

    добавил второй

--- HW_7_03_2023/Triangle.java
+++ HW_7_03_2023/Triangle.java
@@ -2,4 +2,21 @@ package HW_7_03_2023;
 
 public class Triangle {
 
+    public Triangle(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            System.out.println("Стороны не могут быть отрицательные");
+            return;
+        } else if (!checkSides(a, b, c)) {
+            System.out.println("Стороны не образуют треугольник");
+            return;
+        } else {
+            System.out.println("Tреугольник получился");
+            return;
+        }
+    }
+
+    public static boolean checkSides(int a, int b, int c) {
+        return (a + b > c) && (b + c > a) && (a + c > b);
+    }
+
 }
